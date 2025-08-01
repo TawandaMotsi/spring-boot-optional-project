a515fa8a67c9890fa32f5f8cb2dcdcb25c9e7456
emottaw <tawaz08@gmail.com>
2025-08-01 10:07:03 +0100

    new project

---
+++ spring-boot-optional-project/src/test/java/com/example/spring_boot_optional_project/SpringBootOptionalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_optional_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOptionalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
