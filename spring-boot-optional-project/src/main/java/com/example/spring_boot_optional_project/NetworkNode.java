277ac8cf10194b4888e94e995da83ff002561118
emottaw <tawaz08@gmail.com>
2025-08-01 10:21:21 +0100

    NetworkNode.java created

---
+++ spring-boot-optional-project/src/main/java/com/example/spring_boot_optional_project/NetworkNode.java
@@ -0,0 +1,69 @@
+package com.example.spring_boot_optional_project;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class NetworkNode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private int latitude;
+    private int longitude;
+
+    public NetworkNode(Long id, String name, String location, int latitude, int longitude) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+}
+
